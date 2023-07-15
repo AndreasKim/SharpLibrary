60dbeeebb641967ebb7f3a1d8390cdd588b590d8
Andreas Kim <andreas-kim@outlook.de>
2023-07-15 15:31:23 +0200

    introduce dapr introduce redis

---
+++ src/Lending/Infrastructure/Lending.Infrastructure/Repository.cs
@@ -0,0 +1,31 @@
+﻿using NRedisStack;
+using NRedisStack.RedisStackCommands;
+using StackExchange.Redis;
+
+namespace Lending.Infrastructure
+{
+    public class Repository
+    {
+        private readonly IDatabase _db;
+        private readonly JsonCommandsAsync _jsonCommand;
+
+        public Repository()
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            _db = redis.GetDatabase();
+            _jsonCommand = new JsonCommandsAsync(_db);
+        }
+
+        public async Task<bool> Upsert<T>(Guid id, T value)
+        {
+            return await _jsonCommand.SetAsync(new RedisKey(id.ToString()), new RedisValue(typeof(T).Name) , value);
+        }       
+        
+        public async Task<RedisResult> Get<T>(Guid id, T value)
+        {
+            return await _jsonCommand.GetAsync(new RedisKey(id.ToString()), path: new RedisValue(typeof(T).Name));
+        }
+
+
+    }
+}
